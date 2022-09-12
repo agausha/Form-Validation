ce774da8b3f87cf33e75c1c1b7bcc9a5b4f08181
Isaac <uniqucode@gmail.com>
2022-09-12 15:00:53 +0100

    Added storeFormData() functionality

--- js/script.js
+++ js/script.js
@@ -44,7 +44,17 @@ function validateForm() {
   }
 }
 
-function storeFormData() {}
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // Do something with user data
+  console.log(user);
+}
 
 function processFormData(e) {
   e.preventDefault();
